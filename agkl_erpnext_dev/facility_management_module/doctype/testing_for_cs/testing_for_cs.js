ef839c9b5ad33fa91f225844e2997139266b1637
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-10-10 12:40:44 +0530

    commit 2 for the day

--- agkl_erpnext_dev/facility_management_module/doctype/testing_for_cs/testing_for_cs.js
+++ agkl_erpnext_dev/facility_management_module/doctype/testing_for_cs/testing_for_cs.js
@@ -3,6 +3,10 @@
 
 frappe.ui.form.on('testing_for_cs', {
 	// refresh: function(frm) {
-
+	refresh: function(frm)
+	{
+		frm.add_custom_button('button1',{},'button')
+		frm.add_custom_button('button2',{},'button')
+	}
 	// }
 });
--- agkl_erpnext_dev/facility_management_module/doctype/testing_for_cs/testing_for_cs.py
+++ agkl_erpnext_dev/facility_management_module/doctype/testing_for_cs/testing_for_cs.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2023, svr and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class testing_for_cs(Document):
-	pass
+	def _validate(self):
+		# return super()._validate()
+		frappe.msgprint("Hi everyone")
+	# pass
