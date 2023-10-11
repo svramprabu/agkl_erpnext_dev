31ac2b4bf67bb60d7a78247dd3bd9ea054ec1343
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-10-11 20:37:48 +0530

    commit 3 for the day

--- agkl_erpnext_dev/facility_management_module/doctype/testing_doctype/testing_doctype.js
+++ agkl_erpnext_dev/facility_management_module/doctype/testing_doctype/testing_doctype.js
@@ -1,7 +1,14 @@
 // Copyright (c) 2023, svr and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('testing_doctype', {
+// frappe.ui.form.on('testing_doctype', {
+frappe.ui.form.on("testing_doctype", {
+	// 	refresh: function(frm){
+	// 	frm.add_custom_button("Link", function(){
+	// 			var myWin = window.open('https://qrcodescan.in');
+	// 	});
+	// }
+});
 	// refresh: function(frm) {
 	// refresh: function(frm)
 	// {
@@ -23,4 +30,4 @@ frappe.ui.form.on('testing_doctype', {
 	// 		}
 	// 	});
 	// }
-	});
+	// });
--- agkl_erpnext_dev/facility_management_module/doctype/testing_doctype/testing_doctype.py
+++ agkl_erpnext_dev/facility_management_module/doctype/testing_doctype/testing_doctype.py
@@ -5,15 +5,24 @@ import frappe
 from frappe.model.document import Document
 
 class testing_doctype(Document):
+	pass
 
-	def validate(self):
-		self.get_doc_item()
+	# def validate(self):
+	# 	self.get_doc_item()
 
-	def get_doc_item(self):
-		doc = frappe.get_doc('task_queue',self.test_data)
-		for each_mat in doc.get("materials_required"):
-			frappe.msgprint(f"Need {each_mat.name_of_the_material} in {each_mat.quantity}")
-	
+	# def get_doc_item(self):
+	# 	doc = frappe.get_doc('task_queue',self.test_data)
+	# 	for each_mat in doc.get("materials_required"):
+	# 		frappe.msgprint(f"Need {each_mat.name_of_the_material} in {each_mat.quantity}")
+	# x = 
+	# def execute_function(*args,**kwargs):
+    # """
+    # 	This fonction will be executed when the Execute Action Button will be clicked
+    # """
+    	# print('Hello World')
+		# frappe.msgprint("Executed")
+    # The data is transmitted via keyword argument
+    	# print(kwargs)
 	# 	frappe.msgprint("Hiiiii")
         # frappe.msgprint("Hi from server side")
 
