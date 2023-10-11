40410e2ce96a6ba449b79fc2fb44739796df7112
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-10-11 22:32:57 +0530

    commit 4 for the day

--- agkl_erpnext_dev/facility_management_module/doctype/facilitators_list/facilitators_list.py
+++ agkl_erpnext_dev/facility_management_module/doctype/facilitators_list/facilitators_list.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2023, svr and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class facilitators_list(Document):
-	pass
+	def validate(self):
+		# frappe.msgprint(f"{self.name}")
+		self.qr_generator = f"https://api.qrserver.com/v1/create-qr-code/?size=250x250&data={self.name}"
+	# pass
