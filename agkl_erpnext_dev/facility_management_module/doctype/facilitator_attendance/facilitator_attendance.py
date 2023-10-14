4596654109d3e3ac30a989ece9e2f324e3bf41c3
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-10-14 12:06:24 +0530

    commit 1 for the day

--- agkl_erpnext_dev/facility_management_module/doctype/facilitator_attendance/facilitator_attendance.py
+++ agkl_erpnext_dev/facility_management_module/doctype/facilitator_attendance/facilitator_attendance.py
@@ -3,23 +3,13 @@
 
 import frappe
 from frappe.model.document import Document
-# Python program to Scan and Read a QR code 
-from qrtools import QR 
+from qreader import QReader
+# import cv2
  
-
 
 class facilitator_attendance(Document):
 	# pass
 	@frappe.whitelist()
 	def frm_call(self):
 		frappe.msgprint(f"{self.qr_scan}")
-		# doc = frappe.make_get_request(f"http://api.qrserver.com/v1/read-qr-code/?fileurl=\
-		# 					{self.qr_scan}")
-		# frappe.msgprint(f"{doc}")
-		my_QR = QR(filename = f"{self.qr_scan}") 
-
-# decodes the QR code and returns True if successful 
-		my_QR.decode() 
-
-# prints the data 
-		frappe.msgprint (my_QR.data)
+		
