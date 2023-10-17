7079483ff43afcc03254cf7960b68d1afcd85db3
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-10-17 18:37:54 +0530

    Update desktop.py

--- agkl_erpnext_dev/config/desktop.py
+++ agkl_erpnext_dev/config/desktop.py
@@ -4,19 +4,26 @@ from frappe import _
 
 def get_data():
     return [
-        {
-            "module_name": "Agkl Erpnext Dev",
-            "type": "module",
-            "label": _("Agkl Erpnext Dev"),
-            "icon": "octicon octicon-home",
-            "items": [
-                {
-                    "type": "page",
-                    "name": "housekeeping_entry",
-                    "label": _("Housekeeping Entry"),
-                    "icon": "fa fa-suitcase",  # You can set an icon for your custom page
-                    "route": "/housekeeping_entry"  # This should match the route you've defined for your custom page
-                },
-            ]
-        }
-    ]
+		{
+			"module_name": "Agkl Erpnext Dev",
+			"type": "module",
+			"label": _("Agkl Erpnext Dev")
+		}
+	]
+    # return [
+    #     {
+    #         "module_name": "Agkl Erpnext Dev",
+    #         "type": "module",
+    #         "label": _("Agkl Erpnext Dev"),
+    #         "icon": "octicon octicon-home",
+    #         "items": [
+    #             {
+    #                 "type": "page",
+    #                 "name": "housekeeping_entry",
+    #                 "label": _("Housekeeping Entry"),
+    #                 "icon": "fa fa-suitcase",  # You can set an icon for your custom page
+    #                 "route": "/housekeeping_entry"  # This should match the route you've defined for your custom page
+    #             },
+    #         ]
+    #     }
+    # ]
